4a59d5a60bbb36881f43b7ed63f9af4355a31b1f
OlenaPanf <148662268+OlenaPanf@users.noreply.github.com>
2024-03-30 19:32:44 +0000

    Deploying to gh-pages from @ OlenaPanf/goit-js-hw-11@4d54bed9d272f8ea0abbac02311741f8382ce786 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,18 +1,18 @@
-import{S as d,i as u}from"./assets/vendor-7659544d.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&r(n)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const l=document.querySelector(".gallery");function p(i){const s=i.map(r=>`<li class="card">
-   <a class="link" href="${r.largeImageURL}">
+import{i as d,S as u}from"./assets/vendor-7659544d.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();const l=document.querySelector(".gallery"),p=o=>o.map(t=>`<li class="card">
+   <a class="link" href="${t.largeImageURL}">
      <img
-             src="${r.webformatURL}"
-             alt="${r.tags}"
+             src="${t.webformatURL}"
+             alt="${t.tags}"
              width="360"
              height="200"
              class="gallery-img"
          />
          <ul class="list-container">
-         <li class="item-description"><span class="gal-span">Likes</span> ${r.likes}</li>
-         <li class="item-description"><span class="gal-span">Views</span> ${r.views}</li>
-         <li class="item-description"><span class="gal-span">Comments</span> ${r.comments}</li>
-         <li class="item-description"><span class="gal-span">Downloads</span> ${r.downloads}</li>
+         <li class="item-description"><span class="gal-span">Likes</span> ${t.likes}</li>
+         <li class="item-description"><span class="gal-span">Views</span> ${t.views}</li>
+         <li class="item-description"><span class="gal-span">Comments</span> ${t.comments}</li>
+         <li class="item-description"><span class="gal-span">Downloads</span> ${t.downloads}</li>
       </ul>
    </a>
- </li>`).join("");l.insertAdjacentHTML("beforeend",s),new d(".link",{captionsData:"alt",captionDelay:250}).refresh()}function f(i){const s="https://pixabay.com",o="/api/",r=new URLSearchParams({key:"43098974-eee2e6d48134054f86f6d867e",q:i,image_type:"photo",orientation:"horizontal",safesearch:!0}),e=`${s}${o}?${r}`;return fetch(e).then(t=>{if(!t.ok)throw new Error(t.status);return t.json()})}const a=document.querySelector(".form"),c=document.querySelector(".loader"),m=new d(".gallery a",{captionsData:"alt",captionDelay:250});m.refresh();a.addEventListener("submit",i=>{i.preventDefault();const s=a.elements.query.value.trim();if(l.innerHTML='<div class="loader is-hidden"></div>',s===""){u.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!",color:"red",maxWidth:"432px",height:"88px",position:"topRight"});return}c.classList.remove("is-hidden"),f(s).then(o=>{const r=p(o.hits);l.innerHTML=r}).catch(o=>{console.error("Error fetching data:",o)}).finally(()=>{c.classList.add("is-hidden"),a.reset()})});
+ </li>`).join("");function m(o){const t="https://pixabay.com",s="/api/",i=new URLSearchParams({key:"43098974-eee2e6d48134054f86f6d867e",q:o,image_type:"photo",orientation:"horizontal",safesearch:!0}),e=`${t}${s}?${i}`;return fetch(e).then(r=>{if(!r.ok)throw new Error(r.status);return r.json()})}const a=document.querySelector(".form"),c=document.querySelector(".loader");a.addEventListener("submit",o=>{o.preventDefault();const t=a.elements.query.value.trim();if(l.innerHTML='<div class="loader is-hidden"></div>',t===""){d.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!",color:"red",maxWidth:"432px",height:"88px",position:"topRight"});return}c.classList.remove("is-hidden"),m(t).then(s=>{const i=p(s.hits);l.innerHTML=i,new u(".link",{captionsData:"alt",captionDelay:250}).refresh()}).catch(s=>{console.error("Error fetching data:",s)}).finally(()=>{c.classList.add("is-hidden"),a.reset()})});
 //# sourceMappingURL=commonHelpers.js.map
